7f6b368abb50969b30d256594d3beb220881a4e6
st4pzz <ramellajr@gmail.com>
2023-04-27 11:10:25 -0300

    infra inicial

---
+++ demo/src/main/java/Journey/demo/JourneyApplication.java
@@ -0,0 +1,13 @@
+package Journey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/Journey/demo/JourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package Journey.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
